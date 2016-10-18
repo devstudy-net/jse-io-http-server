4cd48f915b9dd191434c934f36db5aa7f508da6d
Devstudy.Net <devstudy.net@gmail.com>
2016-10-18 22:43:09 +0300

    JDBC

--- src/net/devstudy/jse/lection10_jdbc/home/StudentProvider.java
+++ src/net/devstudy/jse/lection10_jdbc/home/StudentProvider.java
@@ -4,28 +4,29 @@ import java.util.List;
 
 import net.devstudy.jse.lection01_classes_objects.Student;
 
+
 /**
- *
+ * 
  * @author devstudy
  * @see http://devstudy.net
  */
 public interface StudentProvider {
 
 	List<Student> findAll(int offset, int limit);
-
+	
 	long countAll();
-
+	
 	Student findById(Long id);
-
+	
 	List<Student> findByAge(int age, int offset, int limit);
-
-	long countByAge(Long id);
-
+	
+	long countByAge(int age);
+	
 	void create(Student student);
-
+	
 	void update(Student student);
-
+	
 	void delete(Student student);
-
+	
 	void deleteById(Long id);
 }
